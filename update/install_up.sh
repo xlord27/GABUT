b857f3908616696227a711ba1605a696fc4ddec6
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-09 20:59:10 +0700

    Update install_up.sh

--- update/install_up.sh
+++ update/install_up.sh
@@ -2,7 +2,7 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-echo -e " [INFO] Downloading Update File"
+echo -e " [INFO] WAITT DATA UPLOADING..."
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/artanodrop/dxavier/main/update/menu-ss.sh" && chmod +x /usr/bin/menu-ss
@@ -12,10 +12,8 @@ wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/xlord27/VPN/m
 wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/artanodrop/dxavier/main/update/menu-bot.sh" && chmod +x /usr/bin/menu-bot
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
 wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu-set.sh" && chmod +x /usr/bin/menu-set
-wget -q -O /usr/bin/clearcache "https://raw.githubusercontent.com/xlord27/gabut/main/menu/clearcache" && chmod +x /usr/bin/clearcache
-wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/xlord27/gabut/main/ssh/usernew" && chmod +x /usr/bin/usernew
 wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/xlord27/gabut/main/backup/menu-backup.sh" && chmod +x /usr/bin/menu-backup
 wget https://raw.githubusercontent.com/xlord27/gabut/main/backup/set-br.sh &&  chmod +x set-br.sh && ./set-br.sh
-echo -e " [INFO] Update Successfully"
+echo -e " [INFO] UPDATE BERHASIL..."
 sleep 2
 exit
