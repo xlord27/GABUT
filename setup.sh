83b7ca3f0a53e31506d4d5d125c0e52917f2fe31
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-11 10:00:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -166,7 +166,7 @@ echo -e "$green          Install XRAY              $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/xlord27/XRAY-XLRD/main/xray.sh && chmod +x xray.sh && ./xray.sh
+wget https://raw.githubusercontent.com/xlord27/lowq/main/xray/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 wget https://raw.githubusercontent.com/xlord27/gabut/main/sshws/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 clear
 
