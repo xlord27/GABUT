0623a1caca5eca9e53b571fa0521e46fcb852b0c
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-11 19:19:43 +0700

    Update xlord-up.sh

--- update/xlord-up.sh
+++ update/xlord-up.sh
@@ -6,7 +6,7 @@ echo -e " [INFO] WAITT DATA UPLOADING..."
 sleep 2
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
-wget -q -O/usr/bin/add-vmess "https://raw.githubusercontent.com/xlord27/lowq/main/xray/add-ws.sh" && chmod +x add-ws
+wget -q -O /usr/bin/add-vmess "https://raw.githubusercontent.com/xlord27/gabut/main/menu/add-vmess.sh" && chmod +x add-vmess /usr/bin/add-vmess
 echo -e " [INFO] UPDATE BERHASIL..."
 sleep 2
 exit
