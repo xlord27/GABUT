fd44bdb760ffd8076a539aa55b5c365437f19a73
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-12 16:27:35 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -273,12 +273,12 @@ cd /usr/bin
 # menu
 wget -O menu "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu.sh"
 wget -O menu-trial "https://raw.githubusercontent.com/xlord27/gabut/main/menu/menu-trial.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/xlord27/lowq/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/xlord27/lowq/main/menu/menu-vless.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/xlord27/gabut/main/xray/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/xlord27/gabut/main/xray/menu-vless.sh"
 wget -O running "https://raw.githubusercontent.com/artanodrop/v4/main/menu/running.sh"
 wget -O clearcache "https://raw.githubusercontent.com/xlord27/gabut/main/menu/clearcache.sh"
 wget -O menu-trgo "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/xlord27/lowq/main/menu/menu-trojan.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/xlord27/gabut/main/xray/menu-trojan.sh"
 wget -O fix "https://raw.githubusercontent.com/xlord27/VPN/main/cf.sh"
 
 # menu ssh ovpn
