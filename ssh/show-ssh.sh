b6dff4d8121559b9a087f8c3d1c16909af8553e6
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-16 05:41:16 +0700

    Update show-ssh.sh

--- ssh/show-ssh.sh
+++ ssh/show-ssh.sh
@@ -130,7 +130,6 @@ echo -e "
 GET / HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]
 " | tee -a /etc/log-create-user.log
 echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
-fi
 echo "" | tee -a /etc/log-create-user.log
 read -n 1 -s -r -p "Press any key to back on menu"
 menu        
