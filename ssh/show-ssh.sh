fc68b8db9eb9143b952853d1acb2e1dbbebf456c
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-16 07:29:39 +0700

    Create show-ssh.sh

---
+++ ssh/show-ssh.sh
@@ -0,0 +1,54 @@
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/ssh/.ssh.db")
+        if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+    echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo -e " \e[1;97;101m                LIST NAME SSH                   \e[0m"
+    echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    echo ""
+    echo "You have no existing clients!"
+    echo ""
+    echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+    read -n 1 -s -r -p "Press [ Enter ] to back on menu ssh"
+    menu
+fi
+
+  echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  echo -e "  \e[1;97;101m        Ketik Angka Untuk Lihat Detail         \E[0m"
+  echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+        echo "     No  User   Exp"
+        grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 2-3 | nl -s ') '
+        until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+                if [[ ${CLIENT_NUMBER} == '1' ]]; then
+                        read -rp "Select one client [1]: " CLIENT_NUMBER
+                else
+                        read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+                fi
+        done
+useradd -e `date -d "$EXPIRED days" +"%Y-%m-%d"` -s /bin/false -M $LOGIN
+exp="$(chage -l $LOGIN | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$PASSWD\n$PASSWD\n"|passwd $LOGIN &> /dev/null
+cat >/var/www/html/ssh-$user.txt <<-END
+END
+echo -e "\e[33m———————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[40;1;37m       XLORD TUNNELING            \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Username    : $LOGIN" | tee -a /etc/log-create-user.log
+echo -e "Password    : $PASSWD" | tee -a /etc/log-create-user.log
+echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[40;1;37m           INFORMASI SERVER      \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
+echo -e "IP          : $IP" | tee -a /etc/log-create-user.log
+echo -e "Domain        : $domain" | tee -a /etc/log-create-user.log
+echo -e "Nameserver  : $sldomain" | tee -a /etc/log-create-user.log
+echo -e "PubKey      : $slkey" | tee -a /etc/log-create-user.log
+echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
+echo -e "OpenSSH     : 22" | tee -a /etc/log-create-user.log
+echo -e "SSH-WS      : 80" | tee -a /etc/log-create-user.log
+echo -e "SSH-SSL-WS  : 443" | tee -a /etc/log-create-user.log
+echo -e "SSL/TLS     : 447 , 777" | tee -a /etc/log-create-user.log
+echo -e "SlowDNS     : 53,5300,443" 
+echo -e "UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
+echo -e "SSH-80      : $domain:80@$LOGIN:$PASSWD"
+echo -e "SSH-443     : $domain:443@$LOGIN:$PASSWD"
+echo -e "SSH-UDP     : 1-65535" | tee -a /etc/log-create-user.log
+echo -e "SETING-UDP  : $domain:1-65535@$LOGIN:$PASSWD"
+echo -e "\e[33m————————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
