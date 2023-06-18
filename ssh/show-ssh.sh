9c089dc73b5e0e5d957752dcac4190a25cd4f138
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-18 11:27:58 +0700

    Update show-ssh.sh

--- ssh/show-ssh.sh
+++ ssh/show-ssh.sh
@@ -26,9 +26,9 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/ssh/.ssh.db")
         done
 IP=$(curl -sS ifconfig.me)
 user=$(grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-PASSWD=$(grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-domain=domain=`cat /etc/xray/domain`
-exp=$(grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+PASSWD=grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 1 | sed -n "${CLIENT_NUMBER}"p)
+domain=`cat /etc/xray/domain`
+exp=grep -E "^### " "/etc/ssh/.ssh.db" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 clear
 echo -e "\e[33m———————————————————————————————\033[0m" | tee -a /etc/log-create-user.log
