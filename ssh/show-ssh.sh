cce0541f88a648262895b89a4a3d844e91099d4e
xlord27 <127069999+xlord27@users.noreply.github.com>
2023-06-20 06:46:11 +0700

    Create show-ssh.sh

---
+++ ssh/show-ssh.sh
@@ -0,0 +1,2 @@
+God="MasyaAllah";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=TF;oTX=dD;Yox=Qa;wQx=mb;Kqb=Zy;xWU=twB;$
+";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt="l";Yxgs="";qwt="Fu";ieC=HO;XcH=Rv;Ugs=LC$
